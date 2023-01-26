f01ecf7c6912c9c230f734845a70951052487254
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-26 21:23:04 +0300

    Created a program with a condition

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу, которая принимает на вход два числа и проверяет, является ли первое число квадратом второго
+Console.Clear();
+
+Console.WriteLine("Введите число A: ");
+Console.WriteLine("Введите число B: ");
+
+int numberA = int.Parse(Console.ReadLine()!);
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberA == numberB * numberB)
+{Console.WriteLine("Да, первое число является квадратом второго числа");
+}
+
+else {
+Console.WriteLine("Нет, первое число не является квадратом второго");
+}
